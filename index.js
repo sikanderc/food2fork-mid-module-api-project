d747771d11727e5e1f1722e8786032be8079e229
sikanderc <sikanderchowhan@gmail.com>
2017-10-19 12:07:31 -0400

    got search working

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-window.onload = function () {
-  // createEventListener()
-  fetch('https://www.googleapis.com/books/v1/volumes?q=')
-  .then(res => res.json())
-  .then(json => displayRecipe(json))
 
-  function displayRecipe(json) {
-    console.log(json)
+
+window.onload = function () {
+  const bookForm = document.getElementById('book-search')
+  bookForm.addEventListener('submit', search)
+
+  function fetchBooks(searched) {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searched}`)
+    .then(res => res.json())
+    .then(json => displayBooks(json))
+  }
+
+  function search(e) {
+    e.preventDefault()
+    let searched = document.getElementById("book-search-input").value
+    fetchBooks(searched)
+    document.getElementById("book-search-input").value = ""
+  }
+
+  function displayBooks(json) {
+    let html = "<ul>"
+    json.items.forEach(e => {
+      let title = e.volumeInfo.title
+      let authors = e.volumeInfo.authors.join(" ")
+      let pictureURL = e.volumeInfo.imageLinks.smallThumbnail
+      html += `<li>Title: ${title}<br>Authors: ${authors}<br>Book Cover:<br><img src='${pictureURL}'><hr></li>`
+    })
+    html += "</ul>"
+    let booksDiv = document.getElementById('books')
+    booksDiv.innerHTML = html
   }
 }
