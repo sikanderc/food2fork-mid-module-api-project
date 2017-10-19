eb80033516896bc3ebb4fe26ac093238ad3cd2b0
sikanderc <sikanderchowhan@gmail.com>
2017-10-19 14:33:38 -0400

    added more functionality and styling

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 window.onload = function () {
   const bookForm = document.getElementById('book-search')
   bookForm.addEventListener('submit', search)
@@ -21,12 +19,44 @@ window.onload = function () {
     let html = "<ul>"
     json.items.forEach(e => {
       let title = e.volumeInfo.title
-      let authors = e.volumeInfo.authors.join(" ")
+      let authors = "unknown"
+      if (e.volumeInfo.authors) {
+        authors = e.volumeInfo.authors.join(", ")
+      }
       let pictureURL = e.volumeInfo.imageLinks.smallThumbnail
-      html += `<li>Title: ${title}<br>Authors: ${authors}<br>Book Cover:<br><img src='${pictureURL}'><hr></li>`
+      let showMore = `<button id="show-button" data-id="${e.id}">Show More</button>`
+      html += `<li>Title: ${title}<br>Authors: ${authors}<br>Book Cover:<br><img src='${pictureURL}'><br>${showMore}</li>`
     })
     html += "</ul>"
     let booksDiv = document.getElementById('books')
     booksDiv.innerHTML = html
+    let showButtons = document.querySelectorAll('#show-button')
+    showButtons.forEach(button => {
+      button.addEventListener('click', fetchBook)
+    })
+  }
+
+  function fetchBook(e) {
+    e.preventDefault()
+    let bookId = e.target.dataset.id
+    fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
+    .then(res => res.json())
+    .then(json => displayBookShow(json))
+  }
+
+  function displayBookShow(json) {
+    let html = "<div>"
+    let title = json.volumeInfo.title
+    let authors = "unknown"
+    if (json.volumeInfo.authors) {
+      authors = json.volumeInfo.authors.join(", ")
+    }
+    let pictureURL = json.volumeInfo.imageLinks.medium
+    let description = json.volumeInfo.description
+
+    html += `<h1>Title: ${title}</h1><h2>Authors: ${authors}</h2><h3>Description: ${description}</h3> <h3>Book Cover:</h3><br><img src='${pictureURL}'><br>`
+    html += "</div>"
+    let showDiv = document.getElementById('show')
+    showDiv.innerHTML = html
   }
 }
